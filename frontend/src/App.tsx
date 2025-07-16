05d063a0cca0553689c0b9f0624a5883898add2a
Adam Planko <adi.planko@gmail.com>
2025-07-16 15:46:42 +0200

    unwrap and delete Main.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,32 @@
 import Header from "./components/Header";
-import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { useState } from "react";
+import { SelectedDayContext, type TDayData } from "./SelectedDayContext";
+import MonthView from "./components/month_overview/MonthView";
+import Tags from "./components/tags_filter/Tags";
+import DayEntriesView from "./components/day_entries_view/DayEntriesView";
+import "./App.css";
 
 function App() {
+  const [selDay, setSelDay] = useState<TDayData>(null);
+
   return (
     <>
       <Header></Header>
-      <Main></Main>
+      <div className="main-content">
+        <SelectedDayContext.Provider
+          value={{
+            data: selDay,
+            setter: setSelDay,
+          }}
+        >
+          <main>
+            <Tags></Tags>
+            <MonthView></MonthView>
+          </main>
+          <DayEntriesView active={true}></DayEntriesView>
+        </SelectedDayContext.Provider>
+      </div>
       <Footer></Footer>
     </>
   );
