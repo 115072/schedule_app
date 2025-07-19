2caf2d3716cf397c39c02c619c6e12bf3747bcb1
Adam Planko <adi.planko@gmail.com>
2025-07-19 10:47:33 +0200

    upgrade to tailwind v4

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,20 @@
 import "@/styles/Header.css";
 
 const Header = () => {
+  const toggleTheme = () => {
+    const rootEl: HTMLElement = document.getElementById("root")!;
+    if (rootEl.classList.contains("dark")) rootEl.classList.remove("dark");
+    else rootEl.classList.add("dark");
+  };
+
   return (
     <header>
+      <button
+        onClick={toggleTheme}
+        className="bg-brand-600 text-neutral-900 m-4"
+      >
+        Toggle Theme
+      </button>
       <h1>Our Schedule App</h1>
     </header>
   );
