010388393f0f9b34c6219be0c33118fa6398a740
Florian Jurik <jurik.florian@gmail.com>
2025-08-15 10:08:55 +0200

    added MiddleWare code. The requests can be protected by authorization from now on

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export function protect(req, res, next) {
+  let token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json("there is no token");
+  } else {
+    if (token.startsWith("Bearer")) {
+      token = token.split(" ")[1];
+      try {
+        const decoded = jwt.verify(token, "mony");
+        console.log(decoded);
+        req.user = decoded;
+        next();
+      } catch (error) {
+        console.error("there was an error: ", error);
+        res.status(401).json("there was an error: ", error);
+      }
+    } else {
+      return res.status(401).json("the sent token is bad");
+    }
+  }
+}
