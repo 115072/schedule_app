307c732f2ee481cc41ee38fdf75c778e711ec1fe
Adam Planko <adi.planko@gmail.com>
2025-08-26 12:18:20 +0200

    fetch days of month from api

--- frontend/src/store/daysSlice.ts
+++ frontend/src/store/daysSlice.ts
@@ -1,66 +1,18 @@
 import type { Event } from "@/utils/types";
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
 import type { RootState } from "./store";
 import { getMonthDays } from "@/utils/translateMonthNum";
+import client from "@/api/client";
 
 //TODO connect to API
 
 export interface DayEvents {
   date: string;
   events: Event[];
-}
-
-function fetchDaysOfMonth(arr: DayEvents[], month: number) {
-  // Fake data
-  //TODO fetch from API
-  arr.splice(0);
-  for (let i = 1; i <= getMonthDays(month); i++) {
-    const day: Date = new Date(2025, month, i + 1);
-    arr.push({
-      date: day.toUTCString(),
-      events:
-        i % month != 0
-          ? []
-          : [
-              {
-                startTimestamp: day.setUTCHours(4, 0),
-                durationMin: 15,
-                description: "This is a test event",
-                tagID: 3,
-              },
-              {
-                startTimestamp: day.setUTCHours(7, 30),
-                durationMin: 120,
-                description: "This is a test event",
-                tagID: 1,
-              },
-              {
-                startTimestamp: day.setUTCHours(8, 30),
-                durationMin: 120,
-                description: "This is a test event",
-                tagID: 2,
-              },
-              {
-                startTimestamp: day.setUTCHours(12, 0),
-                durationMin: 300,
-                description: "This is a test event",
-                tagID: 32,
-              },
-              {
-                startTimestamp: day.setUTCHours(13, 30),
-                durationMin: 160,
-                description: "This is a test event",
-                tagID: 123,
-              },
-              {
-                startTimestamp: day.setUTCHours(14, 30),
-                durationMin: 220,
-                description: "This is a test event",
-                tagID: 12,
-              },
-            ],
-    });
-  }
 }
 
 const initialState: { selDayIdx: number | null; days: DayEvents[] } = {
@@ -72,10 +24,6 @@ export const daysSlice = createSlice({
   name: "days",
   initialState,
   reducers: {
-    fetchMonth: (state, action: PayloadAction<number>) => {
-      fetchDaysOfMonth(state.days, action.payload);
-      state.selDayIdx = 0;
-    },
     setSelDay: (state, action: PayloadAction<number>) => {
       state.selDayIdx = action.payload - 1;
     },
@@ -87,10 +35,68 @@ export const daysSlice = createSlice({
       );
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMonth.pending, () => {
+        console.log("fetching month...");
+      })
+      .addCase(
+        fetchMonth.fulfilled,
+        (state, action: PayloadAction<DayEvents[]>) => {
+          state.days = action.payload;
+          state.selDayIdx = 0;
+
+          console.log(state.days);
+        }
+      );
+  },
 });
 
+export const fetchMonth = createAsyncThunk(
+  "days/fetchMonth",
+  async (month: number) => {
+    let arr: DayEvents[] = [];
+    for (let i = 1; i <= getMonthDays(month); i++) {
+      const day: Date = new Date(2025, month, i + 1);
+      arr.push({
+        date: day.toUTCString(),
+        events: [],
+      });
+    }
+
+    const response = await client({
+      url: `/event/date/${2025}/${month + 1}`,
+      method: "get",
+    });
+
+    for (let [dateStr, evts] of Object.entries(response.data) as [
+      string,
+      Array<any>
+    ][]) {
+      const dayIdx: number = Number(dateStr.split("-")[2]) - 1;
+
+      arr[dayIdx].events = evts.map(
+        (e: any): Event => ({
+          startTimestamp: new Date(arr[dayIdx].date).setUTCHours(
+            e.start.split(":")[0],
+            e.start.split(":")[1]
+          ),
+          durationMin:
+            Number(e.duration.split(":")[0]) * 60 +
+            Number(e.duration.split(":")[1]),
+          description: e.description,
+          //TODO
+          tagID: 1,
+        })
+      );
+    }
+
+    return arr;
+  }
+);
+
 export default daysSlice.reducer;
-export const { fetchMonth, setSelDay, addNewEvent } = daysSlice.actions;
+export const { setSelDay, addNewEvent } = daysSlice.actions;
 
 export const selectDays = (state: RootState) => state.days;
 export const selectSelDay = (state: RootState) => {
