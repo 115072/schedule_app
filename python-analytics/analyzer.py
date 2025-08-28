6548b667ce1295fb7571e44d2d22e0807ef3e7fd
Florian Jurik <jurik.florian@gmail.com>
2025-08-28 16:04:06 +0200

    successfully connected the python module to the database

--- python-analytics/analyzer.py
+++ python-analytics/analyzer.py
@@ -1,14 +1,28 @@
 # 1. Import the FastAPI class
 from fastapi import FastAPI
+import os         # For reading environment variables
+import psycopg2   # For connecting to PostgreSQL
 
 # 2. Create an "instance" of the FastAPI application
 # This 'app' variable is the main point of interaction for our API
 app = FastAPI()
 
-# 3. Define an endpoint using a "decorator"
-# This tells FastAPI that when someone visits the main URL ("/"),
-# the function right below it should handle the request.
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(dbname = os.getenv("DB_NAME"), user = os.getenv("DB_USER"), password = os.getenv("DB_PASS"), host = os.getenv("DB_HOST"))
+        print("Database connection successful!") # It's good to keep this for debugging
+        return conn
+    except Exception as e:
+        print("Database connection failed.")
+        print(e)
+        return None
+
 @app.get("/")
 async def root():
-    # 4. Return a simple JSON response
-    return {"message": "Hello from the Python Analytics Service!"}
+    conn = get_db_connection()
+    if conn:
+        # Don't forget to close the connection when you're done with it!
+        conn.close() 
+        return {"message": "Hello! I successfully connected to the database."}
+    else:
+        return {"message": "Hello! I failed to connect to the database."}
