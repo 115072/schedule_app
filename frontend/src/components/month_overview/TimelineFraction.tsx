7ae7ec1398ff47d86ef825421b62daef64e1c134
PlankoAdam <adi.planko@gmail.com>
2025-11-04 15:31:53 +0100

    add simple tooltip

--- frontend/src/components/month_overview/TimelineFraction.tsx
+++ frontend/src/components/month_overview/TimelineFraction.tsx
@@ -8,21 +8,30 @@ export default function TimelineFraction({ events }: { events: Event[] }) {
   const gridTemplateRows = events.map(() => "1fr").join(" ");
 
   return (
-    <div
-      className="grid"
-      style={{
-        gridTemplateRows,
-      }}
-    >
-      {events.map((e, idx) => {
-        const clr = findTagById(e.tagID, tags)?.color;
-        return (
-          <div
-            style={{ backgroundColor: clr ? clr : "#00000000" }}
-            key={idx}
-          ></div>
-        );
-      })}
+    <div className="relative group block">
+      <div
+        className="grid h-full"
+        style={{
+          gridTemplateRows,
+        }}
+      >
+        {events.map((e, idx) => {
+          const clr = findTagById(e.tagID, tags)?.color;
+          return (
+            <div
+              style={{ backgroundColor: clr ? clr : "#00000000" }}
+              key={idx}
+            ></div>
+          );
+        })}
+      </div>
+      {events.length != 0 ? (
+        <div className="absolute hidden group-hover:block bottom-[120%] left-1/2 -translate-x-1/2 p-2 bg-neutral-100 text-neutral-900 shadow-lg shadow-[rgba(0,0,0,0.50)] rounded-sm">
+          {events.map((e) => {
+            return <div>{e.description}</div>;
+          })}
+        </div>
+      ) : null}
     </div>
   );
 }
