788d930aa074cac98cf4ef4edffdf6c0b74f7153
PlankoAdam <adi.planko@gmail.com>
2025-11-04 15:50:29 +0100

    tooltip follows pointer

--- frontend/src/components/month_overview/TimelineFraction.tsx
+++ frontend/src/components/month_overview/TimelineFraction.tsx
@@ -1,15 +1,35 @@
 import { useAppSelector } from "@/store/hooks";
 import { findTagById, selectTags } from "@/store/tagsSlice";
 import type { Event } from "@/utils/types";
+import { useRef, useState } from "react";
 
 export default function TimelineFraction({ events }: { events: Event[] }) {
   const tags = useAppSelector(selectTags);
-
   const gridTemplateRows = events.map(() => "1fr").join(" ");
 
+  const triggerRef = useRef<HTMLDivElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+  const [visible, setVisible] = useState(false);
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: any) => {
+    if (!triggerRef.current) return;
+    const rect = triggerRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+
+    setPos({
+      x: rect.left + x - (tooltipRef.current?.offsetWidth || 0) / 2,
+      y: rect.top - (tooltipRef.current?.offsetHeight || 0) - 8,
+    });
+  };
+
   return (
-    <div className="relative group block">
+    <div>
       <div
+        ref={triggerRef}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        onMouseMove={handleMouseMove}
         className="grid h-full"
         style={{
           gridTemplateRows,
@@ -26,7 +46,15 @@ export default function TimelineFraction({ events }: { events: Event[] }) {
         })}
       </div>
       {events.length != 0 ? (
-        <div className="absolute hidden group-hover:block bottom-[120%] left-1/2 -translate-x-1/2 p-2 bg-neutral-100 text-neutral-900 shadow-lg shadow-[rgba(0,0,0,0.50)] rounded-sm">
+        <div
+          ref={tooltipRef}
+          className="fixed pointer-events-none p-2 bg-neutral-100 text-neutral-900 shadow-lg shadow-[rgba(0,0,0,0.50)] rounded-sm"
+          style={{
+            left: pos.x,
+            top: pos.y,
+            opacity: visible ? 1 : 0,
+          }}
+        >
           {events.map((e) => {
             return <div>{e.description}</div>;
           })}
